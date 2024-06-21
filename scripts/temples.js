3caadb082b51d47f9da83e9659709a8503993f15
themaxdesigner <osamuyimenigbineweka@gmail.com>
2024-06-21 21:51:21 +0100

    week 2

---
+++ scripts/temples.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.getElementById('hamburger');
+    const navUl = document.querySelector('nav ul');
+
+    hamburger.addEventListener('click', function() {
+        navUl.classList.toggle('active');
+        if (navUl.classList.contains('active')) {
+            hamburger.innerHTML = '&times;'; // Change to 'X' icon
+        } else {
+            hamburger.innerHTML = '&#9776;'; // Change back to hamburger icon
+        }
+    });
+});
